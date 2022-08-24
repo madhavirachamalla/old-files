0c6c6bfa6eda3a49b9e3c0e175d6e272b0882b7e
madhu <madhavirachamalla97@gmail.com>
2022-08-24 10:56:18 +0530

    test

---
+++ bharathi reddy/Program7.java
@@ -0,0 +1,16 @@
+class Program7
+{
+  public static void main(String[] args) 
+	{
+ System.out.println("main method started");
+ char ch1='6';
+ System.out.println(ch1);
+ char ch2=52;
+  System.out.println(ch2);
+ char ch3=1252;
+ System.out.println(ch3);
+ char ch4=32;
+ System.out.println(ch4);
+ System.out.println("main method ended");
+}
+}
